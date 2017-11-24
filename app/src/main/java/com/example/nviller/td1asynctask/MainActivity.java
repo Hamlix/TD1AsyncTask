515806c67d54f3d5b3656d0656c72e473c4e092b
Hamlix <shirohamlix@gmail.com>
2017-11-24 17:00:03 +0100

    Récupération du long clic.

--- app/src/main/java/com/example/nviller/td1asynctask/MainActivity.java
+++ app/src/main/java/com/example/nviller/td1asynctask/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.nviller.td1asynctask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -14,7 +16,7 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    ListView lv;
+    ListView listF;
     ArrayList<HashMap<String, Object>> films;
     SimpleAdapter simpleAdapter;
     static int i = 1;
@@ -25,13 +27,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lv = (ListView) findViewById(R.id.list_films);
+        listF = (ListView) findViewById(R.id.list_films);
         films = new ArrayList<>();
         String[] from = new String[]{"title", "date", "prod"};
         int[] to = {R.id.title_film, R.id.date_film, R.id.prod_film};
 
         simpleAdapter = new SimpleAdapter(this, films, R.layout.listfilms, from, to);
-        lv.setAdapter(simpleAdapter);
+        listF.setAdapter(simpleAdapter);
 
         final Button buttonAdd = (Button) findViewById(R.id.button_add);
         buttonAdd.setOnClickListener(new View.OnClickListener() {
@@ -50,5 +52,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         final Button buttonUpdate = (Button) findViewById(R.id.button_update);
+
+        listF.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(getApplicationContext(), "Changer d'image ici", Toast.LENGTH_SHORT).show();
+                //simpleAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 }
